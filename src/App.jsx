630484e0a27b6b6f74e81831f5afa397bf96fde4
Alok Sharma <alok.sharma61630@gmail.com>
2023-05-26 20:06:31 +0530

    Initial Chatvia setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./assets/sass/main.scss";
+
+const App = () => {
+	return (
+		<div>
+			<h1>This is the Chatvia Application</h1>
+		</div>
+	);
+};
+
+export default App;
